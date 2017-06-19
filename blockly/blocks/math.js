36083e0ea1e89353b08fc725838266478c8ec2ed
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2017-06-19 16:05:04 +0900

    put more math blocks to the toolbox

--- blockly/blocks/math.js
+++ blockly/blocks/math.js
@@ -106,7 +106,7 @@ Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
         "options": [
           ["%{BKY_MATH_SINGLE_OP_ROOT}", 'ROOT'],
           ["%{BKY_MATH_SINGLE_OP_ABSOLUTE}", 'ABS'],
-          ['-', 'NEG'],
+          ["%{BKY_MATH_SUBTRACTION_SYMBOL}", 'NEG'],
           ['ln', 'LN'],
           ['log10', 'LOG10'],
           ['e^', 'EXP'],
@@ -348,11 +348,11 @@ Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
     "type": "math_random_int",
     "message0": "%{BKY_MATH_RANDOM_INT_TITLE}",
     "args0": [
-      {
-        "type": "input_value",
-        "name": "FROM",
-        "check": "Number"
-      },
+      //{
+      //  "type": "input_value",
+      //  "name": "FROM",
+      //  "check": "Number"
+      //},
       {
         "type": "input_value",
         "name": "TO",
